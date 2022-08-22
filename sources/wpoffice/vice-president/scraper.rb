437f5e810fd79f281d122a26520c4f4d235dbeaa
Tony Bowden <tony@tmtm.com>
2022-08-22 16:28:55 +0300

    compare Vice Presidents

---
+++ sources/wpoffice/vice-president/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ img name start end].freeze
+    end
+
+    def empty?
+      (tds[3].text == tds[4].text) || too_early?
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
